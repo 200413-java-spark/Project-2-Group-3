250f34072789b0427c13c0a45c0e950d92e8e037
Sparce Zissef <phuctqle@gmail.com>
2020-05-19 15:14:31 -0500

    spark sql example to order sales of action games by year

--- spark/src/main/java/com/github/p2group3/SparkReader.java
+++ spark/src/main/java/com/github/p2group3/SparkReader.java
@@ -1,5 +1,7 @@
 package com.github.p2group3;
 
+import java.util.function.ToDoubleFunction;
+
 import com.github.p2group3.io.LoadCSV;
 import com.github.p2group3.startup.CreateSparkSession;
 
@@ -8,6 +10,8 @@ import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.DoubleType;
 
 /**
  * Hello world!
@@ -18,19 +22,38 @@ public class SparkReader {
     {
         CreateSparkSession startSession = CreateSparkSession.getInstance(); //Starts SparkSession
         SparkSession session = startSession.getSession(); //pulls a reference to the session
-        if (session != null){
-            System.out.println("A SESSION DOES INDEED EXIST!!!");
-        }
-        String fileName = "vgsales-12-4-2019-short.csv";
+
+        String fileName = "vgsales-12-4-2019.csv";
         //String fileName = "sample.csv";
-        Dataset<Row> data = new LoadCSV().getCSVFileSession(fileName);
+        Dataset<Row> data = new LoadCSV().getCSVFileSession(fileName).cache();
 
         data.printSchema();
 
-        data.createOrReplaceTempView("salesshort");
+        //data.createOrReplaceTempView("salesshort");
 
         //data.show();
 
-        session.sql("SELECT * FROM salesshort WHERE Publisher=\'Nintendo\'").show();
+        Dataset<Row> saleDataDouble1 = data.withColumn("Global_Sales", data.col("Global_Sales").cast(DataTypes.DoubleType));
+        Dataset<Row> saleDataDouble = saleDataDouble1.withColumn("Year", data.col("Year").cast(DataTypes.IntegerType));
+        saleDataDouble.createOrReplaceTempView("salesshort");
+
+        //session.sql("SELECT * FROM salesshort WHERE Publisher=\'Nintendo\'").show();
+        //session.sql("SELECT Name, Publisher, Global_Sales FROM salesshort WHERE Publisher=\'Nintendo\'").show();
+
+        /*Dataset<Row> allGenresSalesDF = session.sql("SELECT Genre, Global_Sales, Year FROM salesshort").na().drop();//drops null values using .na().drop() from table
+        allGenresSalesDF.show();
+        allGenresSalesDF.createOrReplaceTempView("genretable");
+        session.sql("SELECT Genre ,COUNT(*) AS Total_Sales FROM genretable GROUP BY Genre").show();*/
+
+        /*Dataset<Row> actionSalesDF = session.sql("SELECT Global_Sales, Year FROM salesshort WHERE Genre=\'Action\'").na().drop().cache();
+        actionSalesDF.show();
+        actionSalesDF.createOrReplaceTempView("actionGTable");
+        //Dataset<Row> yearActSold = 
+        Dataset<Row> yearGSales = session.sql("SELECT Year ,ROUND(SUM(Global_Sales),2) AS Total_Sales FROM actionGTable GROUP BY Year");
+        yearGSales.createOrReplaceTempView("ysTable");
+        session.sql("SELECT * FROM ysTable ORDER BY Year").show(50);*/
+
+
+
     }
 }
