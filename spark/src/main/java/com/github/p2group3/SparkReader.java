0cafb0f9ce3a446827612e3616b865c785cc020b
Sparce Zissef <phuctqle@gmail.com>
2020-05-20 13:55:25 -0500

    more work on trying to get column merging to work, it still failed

--- spark/src/main/java/com/github/p2group3/SparkReader.java
+++ spark/src/main/java/com/github/p2group3/SparkReader.java
@@ -25,31 +25,44 @@ public class SparkReader {
         Dataset<Row> data = new LoadCSV().getCSVFileSession(fileName).cache();
         data.printSchema();
 
+        //test code block 2
         data.createOrReplaceTempView("dataset0");
-        Dataset<Row> salesCol = session.sql("SELECT Rank, COALESCE(Total_Shipped, Global_Sales) AS Total_Global_Sales FROM dataset0");
+
+        Dataset<Row> data2 = data.drop(data
+        .col("Last_Update")).drop(data.col("url"))
+        .drop(data.col("status")).drop(data
+        .col("img_url"));
+
+        data2.show();
+
+        Dataset<Row> salesCol = session.sql("SELECT " +
+            "COALESCE(Total_Shipped, Global_Sales) " +
+            "FROM dataset0").toDF("Total_Global_Sales"); //combine these two columns
         salesCol.createOrReplaceTempView("salesCol");
-        salesCol.show(300);
-        Column testCol = salesCol.col("Total_Global_Sales").cast(DataTypes.DoubleType);
-
-        Dataset<Row> saleDataDouble1 = data.withColumn("Global_Sales", data.col("Global_Sales").cast(DataTypes.DoubleType));
-        Dataset<Row> saleDataDouble = saleDataDouble1.withColumn("Year", data.col("Year").cast(DataTypes.IntegerType));
+        salesCol.show();
+        session.sql("SELECT COUNT(*) AS Total_Global_Sales FROM salesCol").show();
+        salesCol.printSchema();
+        session.sql("SELECT COUNT(*) AS Total_Shipped FROM dataset0").show();
+        Column testCol = salesCol.col("Total_Global_Sales")
+            .cast(DataTypes.DoubleType);
+        Dataset<Row> salesCol2 = salesCol.withColumn("Total_Global_Sales", testCol);
+        salesCol2.show();
+        /*
+        Dataset<Row> saleDataDouble2 = data.withColumn("Total_Shipped", 
+            data.col("Total_Shipped").cast(DataTypes.DoubleType));
+        Dataset<Row> saleDataDouble1 = saleDataDouble2.withColumn("Global_Sales", 
+            data.col("Global_Sales").cast(DataTypes.DoubleType));
+        Dataset<Row> saleDataDouble = saleDataDouble1.withColumn("Year", 
+            data.col("Year").cast(DataTypes.IntegerType));
         saleDataDouble.createOrReplaceTempView("salesshort");
-
-        saleDataDouble.show(300);
-
+        */
+        Dataset<Row> data3 = data2.withColumn("Total_Global_Sales2", testCol);
+        data3.createOrReplaceTempView("dataset1");   
         
-        Dataset<Row> data2 = data.withColumn("Global_Sales", testCol);
-        data2.createOrReplaceTempView("dataset1");
-        Dataset<Row> data3 = session.sql("ALTER TABLE dataset1 DROP Total_Shipped");
-        data3.show(100);
-        
-        
+        //test code block 1
         //data.createOrReplaceTempView("salesshort");
 
         //data.show();
-
-        
-
 
         //session.sql("SELECT * FROM salesshort WHERE Publisher=\'Nintendo\'").show();
         //session.sql("SELECT Name, Publisher, Global_Sales FROM salesshort WHERE Publisher=\'Nintendo\'").show();
@@ -66,8 +79,5 @@ public class SparkReader {
         Dataset<Row> yearGSales = session.sql("SELECT Year ,ROUND(SUM(Global_Sales),2) AS Total_Sales FROM actionGTable GROUP BY Year");
         yearGSales.createOrReplaceTempView("ysTable");
         session.sql("SELECT * FROM ysTable ORDER BY Year").show(50);*/
-
-
-
     }
 }
