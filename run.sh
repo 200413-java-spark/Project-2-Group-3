1e9900c55a7a3f7eb3d57dbea98f2bd09f6fd778
Sparce Zissef <phuctqle@gmail.com>
2020-05-21 10:50:07 -0500

    prototype of spark app

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-java -jar database/target/database-0.0.1-SNAPSHOT.jar
 java -Xmx1g -jar spark/target/spark-0.0.1-SNAPSHOT.jar
+java -jar database/target/database-0.0.1-SNAPSHOT.jar
+
--- spark/src/main/java/com/github/p2group3/SparkReader.java
+++ spark/src/main/java/com/github/p2group3/SparkReader.java
@@ -1,5 +1,8 @@
 package com.github.p2group3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.p2group3.io.LoadCSV;
 import com.github.p2group3.startup.CreateSparkSession;
 
@@ -9,6 +12,7 @@ import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
 /**
@@ -20,21 +24,103 @@ public class SparkReader {
     {
         CreateSparkSession startSession = CreateSparkSession.getInstance(); //Starts SparkSession
         SparkSession session = startSession.getSession(); //pulls a reference to the session
-
+        //reads in CSV data
         String fileName = "vgsales-12-4-2019.csv";
         Dataset<Row> data = new LoadCSV().getCSVFileSession(fileName).cache();
-        data.printSchema();
-
-        //test code block 2
-        data.createOrReplaceTempView("dataset0");
-
+        //data.printSchema();
+        //drops rows that are not important
         Dataset<Row> data2 = data.drop(data
         .col("Last_Update")).drop(data.col("url"))
         .drop(data.col("status")).drop(data
         .col("img_url"));
 
-        data2.show();
+        data2.createOrReplaceTempView("dataset0");
+        //data2.show();
+        String[] headers = data2.columns();
+        for (int i = 0; i < headers.length; i++){
+            System.out.print(i+" ");
+            System.out.println(headers[i]);
+        }
 
+        int firstH = 3;
+        int seconH = 12;
+
+        Dataset<Row> selectedCategoryPre = session.sql("SELECT "+headers[firstH]+", "
+            +headers[seconH]+", Year FROM dataset0").na().drop();//drops null values using .na().drop() from table
+
+        Dataset<Row> selectedCategory = selectedCategoryPre.withColumn(headers[seconH], 
+            selectedCategoryPre.col(headers[seconH]).cast(DataTypes.DoubleType)).cache();
+
+        //selectedCategory.show();
+        selectedCategory.createOrReplaceTempView("newtable");
+
+        Dataset<Row> sCCount = session.sql("SELECT "+headers[firstH]+
+            " ,COUNT(*) AS New_Column FROM newtable GROUP BY "+headers[firstH]).cache();
+        sCCount.createOrReplaceTempView("tableOfCategories");
+
+        int selectCount = (int) sCCount.count();
+        //System.out.println(selectCount);
+        //sCCount.show();
+        List<Row> list1 = new ArrayList<>();
+        list1 = sCCount.select(headers[firstH]).takeAsList(selectCount);
+
+        //list1.get(0).get(0).toString();
+
+        String option1 = "SUM";
+        String option2 = "AVE";
+
+        List<Dataset<Row>> addList = new ArrayList<>();
+
+        for (int i =0; i< selectCount; i++){
+            //System.out.println(list1.get(i).get(0).toString());
+
+            Dataset<Row> pickTable = session.sql("SELECT "+headers[seconH]+
+            ", Year FROM newtable WHERE "+headers[firstH]+
+            "=\'"+list1.get(i).get(0).toString()+"\'").na().drop().cache();
+            pickTable.createOrReplaceTempView("pickTable");
+            Dataset<Row> yearSales = session.sql("SELECT Year ,ROUND("+option1+"("+headers[seconH]+"),2) AS Totals FROM pickTable GROUP BY Year");
+            yearSales.createOrReplaceTempView("ysTable");
+            Dataset<Row> createdYearsTables = session.sql("SELECT * FROM ysTable ORDER BY Year");
+            addList.add(createdYearsTables);
+        }
+
+        int flag1 = 0;
+        while(flag1 ==0){
+            for (int i =0; i< selectCount; i++){
+                System.out.print(i + "-");
+                System.out.println(list1.get(i).get(0).toString());}
+
+            System.out.print("Pick Row to display: ");
+            int choice = Integer.parseInt(System.console().readLine());
+
+            if (choice == -1) {
+                flag1 = 1;
+            }
+            else{
+                addList.get(choice).show(50);
+                System.out.println(list1.get(choice).get(0).toString());
+            }
+        }
+        //list1.forEach(x -> System.out.println(x.get(0).toString()));
+        /*
+        Dataset<Row> data2Cloned = data2.toDF(headers[0],headers[1],headers[2],
+                headers[3],headers[4],headers[5],
+                headers[6],headers[7],headers[8],
+                headers[9],headers[10],headers[11],
+                headers[12],headers[13],headers[14],
+                headers[15],headers[16],headers[17],
+                headers[18]);*/
+
+        //Dataset<Row> colTest2 = data2.select(functions.coalesce(data2.col("Total_Shipped"),data2.col("Global_Sales"))).toDF("Global_Sales");
+        //Dataset<Row> colTest3 = colTest2.toDF("TG_Sales");
+
+        //Dataset<Row> data3 = data2Cloned.withColumn("Global_Sales", colTest3.col("TG_Sales"));
+
+        //Dataset<Row> data3 = data2.drop("Total_Shipped").drop("Global_Sales").withColumn("Total_Global_Sales", data2.col("Total_Shipped").cast(DataTypes.DoubleType));
+        //data3.printSchema();
+        //data3.show();
+        //test code block 2
+        /*
         Dataset<Row> salesCol = session.sql("SELECT " +
             "COALESCE(Total_Shipped, Global_Sales) " +
             "FROM dataset0").toDF("Total_Global_Sales"); //combine these two columns
@@ -46,7 +132,7 @@ public class SparkReader {
         Column testCol = salesCol.col("Total_Global_Sales")
             .cast(DataTypes.DoubleType);
         Dataset<Row> salesCol2 = salesCol.withColumn("Total_Global_Sales", testCol);
-        salesCol2.show();
+        salesCol2.show();*/
         /*
         Dataset<Row> saleDataDouble2 = data.withColumn("Total_Shipped", 
             data.col("Total_Shipped").cast(DataTypes.DoubleType));
@@ -56,8 +142,10 @@ public class SparkReader {
             data.col("Year").cast(DataTypes.IntegerType));
         saleDataDouble.createOrReplaceTempView("salesshort");
         */
+
+        /*
         Dataset<Row> data3 = data2.withColumn("Total_Global_Sales2", testCol);
-        data3.createOrReplaceTempView("dataset1");   
+        data3.createOrReplaceTempView("dataset1");*/
         
         //test code block 1
         //data.createOrReplaceTempView("salesshort");
