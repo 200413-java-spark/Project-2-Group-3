37c8df454a1e026db38e3655ca5250f93e84484c
Sparce Zissef <phuctqle@gmail.com>
2020-05-22 09:41:44 -0500

    refactoring with column selection

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
+mvn clean compile package
 java -Xmx1g -jar spark/target/spark-0.0.1-SNAPSHOT.jar
-java -jar database/target/database-0.0.1-SNAPSHOT.jar
-
--- spark/src/main/java/com/github/p2group3/ops/Operations.java
+++ spark/src/main/java/com/github/p2group3/ops/Operations.java
@@ -20,37 +20,61 @@ public class Operations {
         //headers for the read in table
         String[] headers = data.columns();
         
-        int firstH = 3;//primary category - string
-        int seconH = 12;//depependent - int/double
-        int thirdH = 17;//independent - int/double
+        System.out.println("Choose your category: \n"+
+        "3 Genre\n"+
+        "4 ESRB_Rating\n"+
+        "5 Platform\n"+
+        "6 Publisher\n"+
+        "7 Developer");
+        int firstH = Integer.parseInt(System.console().readLine());//primary category - string
+        System.out.println("\nChoose your independent variable: \n"+
+        "8 VGChartz_Score\n"+
+        "9 Critic_Score\n"+
+        "10 User_Score\n"+
+        "11 Global_Sales\n"+
+        "12 NA_Sales\n"+
+        "13 PAL_Sales\n"+
+        "14 JP_Sales\n"+
+        "15 Other_Sales\n"+
+        "16 Year\n"+
+        "17 Vgchartzscore\n");
+        int thirdH = Integer.parseInt(System.console().readLine());//independent - int/double
+        System.out.println("\nChoose your dependent variable: \n"+
+        "8 VGChartz_Score\n"+
+        "9 Critic_Score\n"+
+        "10 User_Score\n"+
+        "11 Global_Sales\n"+
+        "12 NA_Sales\n"+
+        "13 PAL_Sales\n"+
+        "14 JP_Sales\n"+
+        "15 Other_Sales\n"+
+        "16 Year\n"+
+        "17 Vgchartzscore\n");
+        int seconH = Integer.parseInt(System.console().readLine());//depependent - int/double
+        System.out.println("\nChoose your function: \n"+
+        "0 SUM\n"+
+        "1 AVG\n");
+        int choice2 = Integer.parseInt(System.console().readLine());//choice of function
+        String[] option1 = {"SUM","AVG"};
 
+        //drops null values using .na().drop() from table
         Dataset<Row> selectedCategoryPre = session.sql("SELECT "+headers[firstH]+", "
-            +headers[seconH]+", "+headers[thirdH]+" FROM dataset0").na().drop();//drops null values using .na().drop() from table
+            +headers[seconH]+", "+headers[thirdH]+" FROM dataset0").na().drop();
 
+        //replaces the independent and dependent variables with the double type for calculations
         Dataset<Row> selectedCategory = selectedCategoryPre
-            .withColumn(headers[seconH], 
-            selectedCategoryPre.col(headers[seconH]).cast(DataTypes.DoubleType))
-            .withColumn(headers[thirdH], 
-            selectedCategoryPre.col(headers[thirdH]).cast(DataTypes.DoubleType))
+            .withColumn(headers[seconH], selectedCategoryPre.col(headers[seconH]).cast(DataTypes.DoubleType))
+            .withColumn(headers[thirdH], selectedCategoryPre.col(headers[thirdH]).cast(DataTypes.DoubleType))
             .cache();
-
-        //selectedCategory.show();
         selectedCategory.createOrReplaceTempView("newtable");
 
+        //creates a string array of all unique values in the category selection
         Dataset<Row> sCCount = session.sql("SELECT "+headers[firstH]+
             " ,COUNT(*) AS New_Column FROM newtable GROUP BY "+headers[firstH]).cache();
-        sCCount.createOrReplaceTempView("tableOfCategories");
-
+        //sCCount.createOrReplaceTempView("tableOfCategories");
         int selectCount = (int) sCCount.count();
-        //System.out.println(selectCount);
-        //sCCount.show();
         List<Row> list1 = new ArrayList<>();
         list1 = sCCount.select(headers[firstH]).takeAsList(selectCount);
-
-        //list1.get(0).get(0).toString();
-
-        String[] option1 = {"SUM","AVG"};
-        int choice2 = 0;
 
         List<Dataset<Row>> addList = new ArrayList<>();
 
@@ -59,9 +83,10 @@ public class Operations {
 
             Dataset<Row> pickTable = session.sql("SELECT "+headers[seconH]+
             ", "+headers[thirdH]+" FROM newtable WHERE "+headers[firstH]+
-            "=\'"+list1.get(i).get(0).toString().replace("'", "\'\'")+"\'").na().drop().cache();
+            "=\'"+list1.get(i).get(0).toString().replace("'", "\'\'")+"\'").cache();
             pickTable.createOrReplaceTempView("pickTable");
-            Dataset<Row> yearSales = session.sql("SELECT "+headers[thirdH]+" ,ROUND("+option1[choice2]+"("+headers[seconH]+"),2) AS Totals FROM pickTable GROUP BY "+headers[thirdH]);
+            Dataset<Row> yearSales = session.sql("SELECT "+headers[thirdH]+
+                " ,ROUND("+option1[choice2]+"("+headers[seconH]+"),2) AS Totals FROM pickTable GROUP BY "+headers[thirdH]);
             yearSales.createOrReplaceTempView("ysTable");
             Dataset<Row> createdYearsTables = session.sql("SELECT * FROM ysTable ORDER BY "+headers[thirdH]);
             addList.add(createdYearsTables);
